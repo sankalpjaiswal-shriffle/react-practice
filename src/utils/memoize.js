5c3ddb5938fb4a5ecd244f00f73f6c104ab387e6
sankalpjaiswal-shriffle <sankalpj@shriffle.com>
2025-12-04 18:46:48 +0530

    changes from the PR

---
+++ src/utils/memoize.js
@@ -0,0 +1,21 @@
+function memoize(func) {
+  let cacheValue = {};
+  let result = 1;
+  return function (...args) {
+    let numKey = JSON.stringify(args); //it will create args as key.
+    if (cacheValue[numKey]) {
+      //if the args key is present then it will return value.
+      console.log("Fetch from cache");
+      return cacheValue[numKey];
+    }
+    for (let i = 0; i < args.length; i++) {
+      result *= args[i];
+    }
+    cacheValue[numKey] = result;
+    return result;
+  };
+}
+
+const memoization = memoize();
+console.log(memoization(2, 3));
+console.log(memoization(2, 3));
