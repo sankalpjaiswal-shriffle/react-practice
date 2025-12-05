0d0de4fca2d5ec9a4ed5be21bf292399b76473bb
sankalpjaiswal-shriffle <sankalpj@shriffle.com>
2025-12-05 19:05:12 +0530

    changes from PR day2

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import ProductCard from "./ProductCard";
-import { productDetails as product } from "../../utils/product";
+import { productDetails } from "../../utils/product";
 
 export default function Product() {
-  const [productDetails, setProductDetails] = useState(product);
   return (
     <div className="m-2 p-2">
       <h1 className="text-3xl text-center font-bold p-3">Product page</h1>
