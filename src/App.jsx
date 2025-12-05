c7a62ed7089abca2747ebb8b9a792b5cf55056e0
sankalpjaiswal-shriffle <sankalpj@shriffle.com>
2025-12-05 18:28:42 +0530

    complete react-day2 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Profile from "./components/Profile";
+import Product from "./components/product/Product";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Profile />
+      <Product />
     </div>
   );
 }
