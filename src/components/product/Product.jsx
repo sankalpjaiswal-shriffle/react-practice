de6513803b73d03fe1674fa91555fe260ac86b00
sankalpjaiswal-shriffle <sankalpj@shriffle.com>
2025-12-08 20:05:25 +0530

    Completed react-day3 task

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -6,7 +6,7 @@ export default function Product() {
   return (
     <div className="m-2 p-2">
       <h1 className="text-3xl text-center font-bold p-3">Product page</h1>
-      {/* <ProductCard productDetails={productDetails} /> */}
+      <ProductCard productDetails={productDetails} />
       <ProductList />
     </div>
   );
