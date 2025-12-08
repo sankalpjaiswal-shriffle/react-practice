c486f655c9b1b114b3e4ab2e5753115f77a10c99
sankalpjaiswal-shriffle <sankalpj@shriffle.com>
2025-12-08 20:00:58 +0530

    Completed react-day3 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Profile from "./components/Profile";
 import Product from "./components/product/Product";
+import Form from "./components/Login";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Profile />
       <Product />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,3 @@
+export default function Loader() {
+  return <div className="flex text-center text-4xl">Loading data...</div>;
+}
