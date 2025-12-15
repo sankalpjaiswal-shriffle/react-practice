182f37a530fc246d81994e1d1f1b9c77da6fa84f
sankalpjaiswal-shriffle <sankalpj@shriffle.com>
2025-12-15 19:24:56 +0530

    Completed react-day8 task

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -4,11 +4,20 @@ import { productApi } from "../../utils/productAPI";
 import { useMemo, useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 import SearchBar from "../common/SearchBar";
+import { Pagination } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { changePage } from "../reducers/paginationSlice";
 
 export default function ProductList() {
-  const limit = 15;
+  const limit = 16;
+  const pageItem = 3;
   const { data, isLoading, error } = useFetch(productApi + `?limit=${limit}`);
+  const currentPage = useSelector((state) => state.pagination.currentPage);
+  const dispatch = useDispatch();
   const [search, setSearch] = useState(null);
+  const page = Math.ceil(data?.products.length / pageItem);
+  const lastIndex = currentPage * pageItem;
+  const firstIndex = lastIndex - pageItem;
 
   const filteredList = useMemo(() => {
     let trimed = search?.trim().toLowerCase();
@@ -21,6 +30,14 @@ export default function ProductList() {
     );
   }, [data, search]);
 
+  const pageData = search
+    ? filteredList
+    : data?.products.slice(firstIndex, lastIndex);
+
+  function handlePageChange(page) {
+    dispatch(changePage(page));
+  }
+
   if (error)
     return <div className="flex items-center justify-center">{error}</div>;
 
@@ -29,7 +46,18 @@ export default function ProductList() {
   return (
     <div className="p-2 m-2 dark:bg-black">
       <SearchBar setSearch={setSearch} />
-      <ProductCard productList={filteredList} />
+      <ProductCard productList={pageData} />
+      {!search && (
+        <Pagination
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          count={page}
+          onChange={(_, page) => handlePageChange(page)}
+        />
+      )}
     </div>
   );
 }
